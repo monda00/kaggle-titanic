cf995da755a9391819088040789b383a93066619
恩田雅大 <masa@masa-2.local>
2019-01-05 20:56:02 +0900

    update compareModel.py add Title and FamilySize data

--- src/compareModel.py
+++ src/compareModel.py
@@ -31,18 +31,24 @@ combine = [train_df, test_df]
 まずは、データ分析から
 '''
 
+####
 # 特徴量の確認
+####
 print(train_df.columns.values)
 print(train_df.head())
 
+####
 # データの情報確認
+####
 print(train_df.info())
 print('_'*40)
 print(test_df.info())
 print('_'*40)
 print(train_df.describe())
 
+####
 # 特徴量の解析
+####
 print("チケットクラスが生存に関係するか")
 print(train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
 print("性別が生存に関係するか")
@@ -54,19 +60,99 @@ print(train_df[['Pclass', 'Sex', 'Survived']].groupby(['Pclass', 'Sex'], as_inde
 # 年齢（数値特徴量の相関）
 # grid = sns.FacetGrid(train_df, col='Survived')
 # grid.map(plt.hist, 'Age', bins=20)
-
 # チケットクラスと年齢（順序尺度と数値特徴量の相関）
 # grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', height=2.2, aspect=1.6)
 # grid.map(plt.hist, 'Age', alpha=.5, bins=20)
 # grid.add_legend()
-
 # 出港地（カテゴリカル特徴量の相関）
 # grid = sns.FacetGrid(train_df, row='Embarked', height=2.2, aspect=1.6)
 # grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
 # grid.add_legend()
-
 # 出港地と性別と運賃（カテゴリカル特徴量と数値の相関）
 # grid = sns.FacetGrid(train_df, row='Embarked', col='Survived', height=2.2, aspect=1.6)
 # grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
 # grid.add_legend()
 # plt.show()
+
+####
+# データの調整
+####
+
+# TicketとCabinの特徴量を削除
+train_df = train_df.drop(['Ticket', 'Cabin'], axis=1)
+test_df = test_df.drop(['Ticket', 'Cabin'], axis=1)
+combine = [train_df, test_df]
+
+# Title（肩書）を抽出
+for dataset in combine:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
+    'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+print("肩書が生存に関わるか")
+print(train_df[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+# NameとPassengerIdを削除
+train_df = train_df.drop(['Name', 'PassengerId'], axis=1)
+test_df = test_df.drop(['Name'], axis=1)
+combine = [train_df, test_df]
+
+# 性別を数値に変換
+for dataset in combine:
+    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+# 年齢の欠損値を補完
+guess_ages = np.zeros((2,3))
+
+for dataset in combine:
+    for i in range(0, 2):
+        for j in range(0, 3):
+            guess_df = dataset[(dataset['Sex'] == i) & \
+                                  (dataset['Pclass'] == j+1)]['Age'].dropna()
+
+            # age_mean = guess_df.mean()
+            # age_std = guess_df.std()
+            # age_guess = rnd.uniform(age_mean - age_std, age_mean + age_std)
+
+            age_guess = guess_df.median()
+
+            # Convert random age float to nearest .5 age
+            guess_ages[i,j] = int( age_guess/0.5 + 0.5 ) * 0.5
+
+    for i in range(0, 2):
+        for j in range(0, 3):
+            dataset.loc[ (dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j+1),\
+                    'Age'] = guess_ages[i,j]
+
+    dataset['Age'] = dataset['Age'].astype(int)
+
+train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
+print(train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True))
+
+for dataset in combine:
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
+
+train_df = train_df.drop(['AgeBand'], axis=1)
+combine = [train_df, test_df]
+
+# 既存の特徴量を組み合わせてFamilySizeを作成する
+for dataset in combine:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+
+print("家族データは生存に関係するか")
+print(train_df[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean().sort_values(by='Survived', ascending=False))
